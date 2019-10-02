e099ed490eab9b155459c67095eb733707a8489c
muhammad-majid <42956457+muhammad-majid@users.noreply.github.com>
2019-10-02 10:44:03 +1000

    adf

--- BatchInstallation/choco_batch_Installation.bat
+++ BatchInstallation/choco_batch_Installation.bat
@@ -1,4 +1,4 @@
-#Install choco .exe and add choco to PATH
+:: Install choco .exe and add choco to PATH
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-#Install all the packages
+:: Install all the packages
 for /F "eol=#" %%G in (packages.txt) do choco upgrade %%G -y
